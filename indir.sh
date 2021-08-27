ea18f92497c7bdae3172178be4d90edd5ad633b1
Aspers <74264874+asper-tool@users.noreply.github.com>
2021-08-27 09:00:20 +0300

    Add files via upload

---
+++ indir.sh
@@ -0,0 +1,25 @@
+echo -e $red " ~~~~~~~~~~~ Gerekli Araçlar İndiriliyor ~~~~~~~~~~~~$tp"
+echo ""
+
+git clone https://github.com/asper-tool/Mys-Tor
+
+cd Mys-Tor
+
+echo -e $red " ~~~~~~~~~~~ İp Değişim için Gerekenler Yapılıyor... ~~~~~~~~~~~~$tp"
+echo ""
+ 
+
+python2 otomatik.py 
+
+echo ""
+
+
+
+
+                echo " Mys-ip Programini Tercih Ettiginiz Icin Teseskkurler {Mys} !!" 
+                echo ""
+                exit 0
+done
+
+
+
