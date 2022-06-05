c9ac4bbe2f75be77ef1d7191fdaad31d9369687d
Aspers <74264874+asper-tool@users.noreply.github.com>
2022-06-06 02:52:57 +0300

    Update indir.sh

--- indir.sh
+++ indir.sh
@@ -1,3 +1,15 @@
+clear
+echo "******* Torghost installer ********"
+echo ""
+echo "=====> Installing tor bundle "
+sudo apt-get install tor -y -qq
+echo "=====> Installing dependencies "
+sudo pip install stem
+echo "=====> Installing TorGhost "
+sudo cp torghost /usr/bin/torghost
+sudo chmod +x /usr/bin/torghost
+echo "=====> Done "
+
 echo -e $red " ~~~~~~~~~~~ Gerekli Araçlar İndiriliyor ~~~~~~~~~~~~$tp"
 echo ""
 
